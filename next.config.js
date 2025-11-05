5038b817cf4d7f195acaca434ab56b2b259b472e
marc100s <marc@ellipsi.net>
2025-11-05 17:14:37 +0100

    chore: updated dependencies

--- next.config.js
+++ next.config.js
@@ -1,26 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverExternalPackages: ["mongoose"],
-    experimental: {
+  turbopack: true,
+  serverExternalPackages: ["mongoose"],
+  experimental: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
 
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'lh3.googleusercontent.com',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      }
-      return config
-    }
-  }
-  
-  module.exports = nextConfig
+module.exports = nextConfig;
